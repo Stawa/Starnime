5df15783cdd6c77acbd5c0f0ad31a1b65884e0ec
Stawa <69102292+Stawa@users.noreply.github.com>
2023-06-16 19:01:28 +0800

    Updated typings, added options

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,52 +1,69 @@
 declare module "starnime" {
-    enum Version {
-        V1 = "v1",
-    }
+	enum Version {
+		V1 = "v1",
+	}
 
-    enum Category {
-        ICONS = "icons",
-        SFW = "sfw"
-    }
+	enum Category {
+		ICONS = "icons",
+		SFW = "sfw",
+	}
 
-    enum Attachment {
-        BOOK = "book",
-        CLOCK = "clock",
-        CROSS = "cross",
-        CORRECT = "correct",
-        EMPTY = "empty",
-        FILL = "fill",
-        FREQUENCY = "frequency",
-        FFMPEG = "ffmpeg",
-        LOGIN = "login",
-        LOGOUT = "logout",
-        NEXT = "next",
-        NOTES = "notes",
-        PAUSE = "pause",
-        PHOTOS = "photos",
-        PLAY = "play",
-        PLUS = "plus",
-        PREVIOUS = "previous",
-        REFRESH = "refresh",
-        RIGHT = "right",
-        SETTINGS = "settings",
-        STAR = "star",
-        UP = "up",
-        VERIFIED = "verified",
-    }
+	enum Icons {
+		BOOK = "book",
+		CLOCK = "clock",
+		CROSS = "cross",
+		CORRECT = "correct",
+		EMPTY = "empty",
+		FILL = "fill",
+		FREQUENCY = "frequency",
+		FFMPEG = "ffmpeg",
+		LOGIN = "login",
+		LOGOUT = "logout",
+		NEXT = "next",
+		NOTES = "notes",
+		PAUSE = "pause",
+		PHOTOS = "photos",
+		PLAY = "play",
+		PLUS = "plus",
+		PREVIOUS = "previous",
+		REFRESH = "refresh",
+		RIGHT = "right",
+		SETTINGS = "settings",
+		STAR = "star",
+		UP = "up",
+		VERIFIED = "verified",
+	}
 
-    function get(version: string, category: string, params: string): Promise<Object>;
+	function get(
+		version: string,
+		category: string,
+		params: string
+	): Promise <Object>;
+
+	class Options {
+		skipMissingError: boolean;
+	}
 
 	class ApiResponse {
 		url: string;
 		code: number;
+		error: {
+			name: string;
+			message: string;
+			code: string;
+		};
 		constructor(response: {
-			url: string; code: number
+			url: string, code: number, error: object
 		});
 	}
 
 	class Starnime {
-        constructor();
-		image(version: string, attachment: string): Promise < ApiResponse > ;
+		constructor(options?: Options);
+		image(
+			version: string,
+			category: string,
+			endpoint: string
+		): Promise <ApiResponse>;
 	}
 
 	class StarError extends Error {
@@ -59,8 +76,9 @@ declare module "starnime" {
 		ApiResponse,
 		Starnime,
 		StarError,
-		Attachment,
+        Options,
+		Icons,
 		Category,
-		Version,
+		Version
 	};
 }
